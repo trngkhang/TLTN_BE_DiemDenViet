ecd264d529d2201e00f2764ecd447e80377b21b5
trngkhang <130826168+trngkhang@users.noreply.github.com>
2025-01-04 09:13:12 +0700

    Update genfromDB.js

--- src/services/AImodel/genfromDB.js
+++ src/services/AImodel/genfromDB.js
@@ -4,13 +4,13 @@ const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-exp-1206",
+  model: "gemini-1.5-pro",
 });
 
 const generationConfig = {
   temperature: 1,
   topP: 0.95,
-  topK: 64,
+  topK: 40,
   maxOutputTokens: 8192,
   responseMimeType: "application/json",
 };
